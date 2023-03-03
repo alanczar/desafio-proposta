b31ada82f4a2cb2a55ca2fd7d4aa4174d419a0b7
alfiguei <alan.figueiredo@capgemini.com>
2023-03-03 15:36:53 -0300

    desafio-proposta

--- main.js
+++ main.js
@@ -3,20 +3,54 @@ const fs = require("fs");
 const utils = require("./utils");
 const port = 3003;
 console.log("Starting app on port:", port);
+
 const app = express();
 
 app.use(express.json());
 
+// Definindo um endpoint POST que recebe uma lista de propostas para validação
 app.post("/api/proposta/validar", function (req, res) {
 
+    // Extrai a lista de propostas do corpo da requisição
     const proposals = req.body.propostas;
 
+    // Imprime a lista de propostas recebidas no console
     console.log("propostas received:", JSON.stringify(proposals));
 
+    // Salva as propostas válidas em um arquivo JSON usando uma função do módulo 'utils'
     const result = utils.saveFileValidProposals('./json/proposals.json', proposals);
 
+    // Imprime o resultado da validação no console (incluindo as propostas inválidas)
     console.log("result:", JSON.stringify(result));
-    res.json(result);
 
-})
+    // Retorna o resultado da validação como um objeto JSON
+    res.json(result);
+});
+
+// Definindo um endpoint GET que recebe o número de proposta e retorna o objeto 'produto'
+app.get('/api/proposta/:proposta/produto', function(req, res) {
+
+  // Extrai o número da proposta dos parâmetros da URL
+  const proposalId = req.params.proposta;
+
+  // Carrega as propostas válidas do arquivo JSON usando uma função do módulo 'utils'
+  const proposals = utils.loadValidProposals('./json/proposals.json');
+
+  // Encontra a proposta com o 'propostaId' correspondente
+  const proposal = proposals.find(p => p.propostaId === parseInt(proposalId));
+
+  // Se a proposta não for encontrada, retorna uma mensagem de erro com status 404
+  if (!proposal) {
+    res.status(404).send('Proposal not found');
+    return;
+  }
+
+  // Extrai o objeto 'produto' da proposta encontrada
+  const produto = proposal.produto;
+
+  // Retorna o objeto 'produto' como um objeto JSON
+  res.json({ produto });
+});
+
+// Inicia o servidor na porta especificada
 app.listen(port);
--- utils.js
+++ utils.js
@@ -1,18 +1,20 @@
 const fs = require('fs');
 
 const validateProposalId = (proposal) => {
-
     proposal = String(proposal);
 
+    // Extrai os dois últimos dígitos do código da proposta
     console.log("separates the check code");
     const code = proposal.substring(proposal.length - 2);
     console.log("code", code);
 
+    // Separa os outros dígitos do código da proposta em um array
     console.log("separate the other digits");
-    const arrNumbers = proposal.split("", 8);
+    const arrNumbers = proposal.split("");
     var sumEven = 0;
     var sumOdd = 0;
 
+    // Soma os valores dos dígitos pares e ímpares
     console.log("Add the even and odd values");
     arrNumbers.forEach(element => {
         if (element % 2 === 0) {
@@ -24,6 +26,7 @@ const validateProposalId = (proposal) => {
 
     console.log("even", sumEven, "odd", sumOdd);
 
+    // Calcula o código de verificação a partir da diferença entre a soma dos dígitos pares e ímpares
     var verifyCode = 0;
     if (sumEven > sumOdd) {
         verifyCode = (sumEven - sumOdd) / 2;
@@ -34,17 +37,22 @@ const validateProposalId = (proposal) => {
     verifyCode = Math.round(verifyCode);
 
     console.log("verifyCode", verifyCode);
-    return code === verifyCode;
+    return code === String(verifyCode);
 };
 
+// Função para salvar as propostas em um arquivo e contar quantas são válidas e inválidas
 const saveFileProposals = (path, proposals) => {
-
     let countValid = 0;
     let countInvalid = 0;
 
+    // Itera sobre as propostas
     proposals.forEach(element => {
         let verifyCode = "00" + element.produto.nrVersaoOferta
+
+        // Concatena o código da proposta com o código de verificação
         let proposal = element.propostaId + verifyCode.substring(verifyCode.length - 2);
+
+        // Valida a proposta e conta se é válida ou inválida
         if (validateProposalId(proposal)) {
             console.log("proposal:", proposal, "Valid");
             countValid++;
@@ -54,6 +62,7 @@ const saveFileProposals = (path, proposals) => {
         }
     })
 
+    // Escreve as propostas em um arquivo
     fs.writeFileSync(path, JSON.stringify(proposals));
 
     console.log("valid:", countValid, "invalid:", countInvalid);
@@ -63,5 +72,13 @@ const saveFileProposals = (path, proposals) => {
     }
 }
 
+// Função para carregar as propostas válidas de um arquivo
+const loadValidProposals = (path) => {
+    const fileData = fs.readFileSync(path, 'utf8');
+    const proposals = JSON.parse(fileData);
+    return proposals.filter(p => validateProposalId(p.propostaId));
+}
+
+// Exporta as funções para serem usadas em outros arquivos
 module.exports.validateProposalId = validateProposalId;
 module.exports.saveFileValidProposals = saveFileProposals;
